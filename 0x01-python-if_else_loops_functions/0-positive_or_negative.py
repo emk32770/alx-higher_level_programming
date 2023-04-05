944da1271c0c4e1362b54904faea91df53965f81
emk32770 <117989870+emk32770@users.noreply.github.com>
2023-04-05 16:09:15 +0000

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-print(number, "is", "zero" if number == 0 else "negative"
-      if number < 0 else "positive")
--- 0x01-python-if_else_loops_functions/7-islower.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def islower(c):
-    return (ord(c) in range(ord("a"), ord("z")+1))
